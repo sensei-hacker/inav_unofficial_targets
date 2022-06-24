dd4fbdf5a78b6bcbcd5947442f871b22fe7222a4
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2022-06-24 15:32:08 +0300

    adjusted simulator command

--- src/main/drivers/display.c
+++ src/main/drivers/display.c
@@ -178,16 +178,14 @@ int displayWriteChar(displayPort_t *instance, uint8_t x, uint8_t y, uint16_t c)
         displayUpdateMaxChar(instance);
     }
 
-#ifdef USE_SIMULATOR
 	if (ARMING_FLAG(SIMULATOR_MODE)) {
 		//some FCs do not power max7456 from USB power 
-		//driver can not read font metadata 
+		//driver can not read front metadata 
 		//chip assumed to not support second bank of font
-		//artifical horizon, variometer and home direction are not drawn ( display.c: displayUpdateMaxChar())
-		//return dummy metadata to let all OSD elements to work in simulator mode
+		//artifical horizon, variometer and home direction are not drawn ( displat.c: displayUpdateMaxChar())
+		//return dummy metadata so OSD work in simulator mode
 		instance->maxChar = 512;
 	}
-#endif
 
     if (c > instance->maxChar) {
         return -1;
