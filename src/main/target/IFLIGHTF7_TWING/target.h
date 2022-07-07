ffa8a378e1cc9eee293c763496df19e36e256e54
JuliooCesarMDM <juliomatias123@outlook.com>
2022-07-07 19:49:14 -0300

    Remove USE_EXTI, USE_MPU_DATA_READY_SIGNAL and DEBUG_MPU_DATA_READY_INTERRUPT defines

--- src/main/target/IFLIGHTF7_TWING/target.h
+++ src/main/target/IFLIGHTF7_TWING/target.h
@@ -47,6 +47,10 @@
 
 #define MPU6500_1_CS_PIN        PA15
 #define MPU6500_1_SPI_BUS       BUS_SPI1
+#define MPU6500_1_EXTI_PIN      PA8
+
+
+
 
 // *************** I2C/Baro/Mag *********************
 #define USE_I2C
