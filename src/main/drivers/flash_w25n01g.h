cdf70c4f664f38779cca15cf77b88732be70646e
Julio Cesar Matias <juliomatias123@outlook.com>
2022-07-11 05:00:51 -0300

    Support for Flash Memory W25N01G of 128MB (#8166)
    * Add support for W25N01G flash memory
    
    * operating attempt
    
    * push improvs
    
    * fix compilation
    
    * maybe a final code
    
    * tentative for fix compilation
    
    * second attempt to fix the problem
    
    * third attempt to fix the build
    
    * format extension
    
    * fix machine cycle lockup
    
    * final push
    
    * push flash detection fixes
    
    * verify if flash pointer is null
    
    * increase project version
    
    * push improvs
    
    * maybe a final code
    
    * tentative for fix compilation
    
    * second attempt to fix the problem
    
    * third attempt to fix the build
    
    * format extension
    
    * final push
    
    * push flash detection fixes
    
    * verify if flash pointer is null
    
    * increase project version
    
    * fix compilation

--- src/main/drivers/flash_w25n01g.h
+++ src/main/drivers/flash_w25n01g.h
@@ -1,13 +1,13 @@
 /*
- * This file is part of INAV.
+ * This file is part of iNav.
  *
- * INAV are free software. You can redistribute
+ * iNav are free software. You can redistribute
  * this software and/or modify this software under the terms of the
  * GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
- * INAV are distributed in the hope that they
+ * iNav are distributed in the hope that they
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
