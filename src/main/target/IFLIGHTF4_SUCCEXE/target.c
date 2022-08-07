ec66d51e997ad98790fabbaf858470e6f74149dc
Ernest Stepanov (ErNis) <ernis@fpvdev.com>
2022-08-07 10:03:40 +0300

    Target IFLIGHTF4_SUCCEXE added

--- src/main/target/IFLIGHTF4_SUCCEXE/target.c
+++ src/main/target/IFLIGHTF4_SUCCEXE/target.c
@@ -1,4 +1,5 @@
-/*SYNERDUINO
+/*
+>>>>>>> 7147dc824 (Target IFLIGHTF4_SUCCEXE added)
 * This file is part of Cleanflight.
 *
 * Cleanflight is free software: you can redistribute it and/or modify
@@ -24,27 +25,14 @@
 #include "drivers/pinio.h"
 
 timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM2, CH4, PA3,  TIM_USE_PPM,         1, 1), // D(1, 6, 3)
 
-/**Motors & Servos for FW & MC**/
+    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_MC_MOTOR,    0, 0), // D(1, 7, 5)
+    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_MC_MOTOR,    0, 0), // D(1, 2, 5)
+    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_MC_MOTOR,    0, 0), // D(2, 7, 7)
+    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_MC_MOTOR,    0, 0), // D(2, 2, 0)
 
-    DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR, 0, 1), // S1 D(2,1,6)
-    DEF_TIM(TIM2,  CH2, PB3,  TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR, 0, 0), // S2 D(1,6,3)
-    DEF_TIM(TIM2,  CH3, PB10, TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 0), // S3 D(1,1,3)
-    DEF_TIM(TIM2,  CH1, PA15, TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 0), // S4 D(1,5,3)
-    DEF_TIM(TIM5,  CH1, PA0,  TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 0), // S5 D(1,2,6)
-    DEF_TIM(TIM3,  CH1, PB4,  TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 0), // S6 D(1,4,5)
-    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_FW_SERVO, 0, 0),                    // S7 D(1,7,5)
-
-/**Softserial**/
-
-    DEF_TIM(TIM4,  CH1, PB6,  TIM_USE_ANY, 0, 0), //Softserial1_TX
-    DEF_TIM(TIM4,  CH2, PB7,  TIM_USE_ANY, 0, 0), //Softserial1_RX
-
-/**LED STRIP WS2811**/
-
-    DEF_TIM(TIM3,  CH2, PB5,  TIM_USE_LED, 0, 0), //2811LED D(1,5,5)
-
-
+    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_LED,         0, 0), // D(1, 0, 2)
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
