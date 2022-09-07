5289459255f4895bd016de7a642cb65f4d1d7d9a
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2022-09-07 16:10:56 +0200

    MambaF772_WING

--- src/main/target/MAMBAF722_WING/target.h
+++ src/main/target/MAMBAF722_WING/target.h
@@ -167,5 +167,5 @@
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
-#define PINIO1_PIN                  PC0
-#define PINIO2_PIN                  PC2
+#define PINIO1_PIN                  PC2
+#define PINIO2_PIN                  PC0
