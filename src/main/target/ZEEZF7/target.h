56c2c297ac5148bbf7bdda8b8090a1cbd51791e8
Darren Lines <darren@darrenlines.uk>
2022-09-19 18:31:43 +0100

    Disabled stick commands when in CLI
    This disabled stick commands when in CLI. Ideally they should be disabled all the time when connected to configurator. An issue has been discovered where the accelerometer can be trimmed via stick commands while in configurator. If this happens too much, it will cause a hang, and wipe all settings.
    For testing, I added a `SYSTEM_MESSAGE` to the OSD to show when settings had been saved. This is pretty useful, so it may as well stay. Maybe this will reduce batteries being pulled out too early.

--- src/main/target/ZEEZF7/target.h
+++ src/main/target/ZEEZF7/target.h
@@ -79,6 +79,7 @@
 
 #define MPU6000_CS_PIN          SPI1_NSS_PIN
 #define MPU6000_SPI_BUS         BUS_SPI1
+#define GYRO_INT_EXTI           PC4
 #define IMU_MPU6000_ALIGN       CW0_DEG
 #define USE_IMU_MPU6000
 #endif
