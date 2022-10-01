222790444ac239da9fcdc9b244ff3b089b19f5a0
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2022-10-01 11:22:04 +0200

    Rename HDzero to MSP displayport

--- src/main/target/IFLIGHT_JBF7PRO/target.h
+++ src/main/target/IFLIGHT_JBF7PRO/target.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of Cleanflight.
+ * This file is part of INAV Project.
  *
- * Cleanflight is free software: you can redistribute it and/or modify
+ * INAV Project is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Cleanflight is distributed in the hope that it will be useful,
+ * INAV Project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ * along with INAV Project.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -27,10 +27,6 @@
 #define BEEPER                  PC13
 #define BEEPER_INVERTED
 
-// *************** IMU generic ***********************
-#define USE_EXTI
-#define USE_MPU_DATA_READY_SIGNAL
-
 // *************** Gyro & ACC **********************
 #define USE_SPI
 #define USE_SPI_DEVICE_1
@@ -43,13 +39,11 @@
 #define IMU_MPU6000_ALIGN       CW0_DEG
 #define MPU6000_SPI_BUS         BUS_SPI1
 #define MPU6000_CS_PIN          PA4
-#define MPU6000_EXTI_PIN        PC4
 
 #define USE_IMU_BMI270
 #define IMU_BMI270_ALIGN        CW0_DEG
 #define BMI270_SPI_BUS          BUS_SPI1
 #define BMI270_CS_PIN           PA4
-#define BMI270_EXTI_PIN         PC4
 
 // *************** I2C/Baro/Mag *********************
 #define USE_I2C
@@ -150,10 +144,10 @@
 #define USE_RANGEFINDER
 #define RANGEFINDER_I2C_BUS         BUS_I2C1
 
-#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_CURRENT_METER | FEATURE_TELEMETRY | FEATURE_VBAT | FEATURE_OSD)
+#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_CURRENT_METER | FEATURE_TELEMETRY | FEATURE_VBAT | FEATURE_OSD | FEATURE_LED_STRIP)
 
 #define USE_LED_STRIP
-#define WS2811_PIN              PA8  // TIM2_CH1
+#define WS2811_PIN              PA8
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
