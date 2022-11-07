iNavFlight/inav

Mamba H743_2022B fixes

src/main/target/MAMBAH743/target.h
@@ -63,7 +63,7 @@
 // #define SPI4_MOSI_PIN           PE14
 
 #define USE_IMU_ICM42605
-#define IMU_ICM42605_ALIGN      CW90_DEG_FLIP
+#define IMU_ICM42605_ALIGN      CW90_DEG
 #define ICM42605_SPI_BUS        BUS_SPI1
 #define ICM42605_CS_PIN         PA4
 #define ICM42605_EXTI_PIN       PC4
@@ -175,11 +175,22 @@
 
 #define DEFAULT_RX_TYPE RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER SERIALRX_SBUS
-#define SERIALRX_UART SERIAL_PORT_USART6
 
 // *************** ADC *****************************
 #define USE_ADC
 #define ADC_INSTANCE ADC3
+
+#ifdef MAMBAH743_2022B
+
+#define ADC_CHANNEL_1_PIN PC1
+#define ADC_CHANNEL_2_PIN PC3
+#define ADC_CHANNEL_3_PIN PC0
+
+#define VBAT_ADC_CHANNEL ADC_CHN_1
+#define CURRENT_METER_ADC_CHANNEL ADC_CHN_2
+#define AIRSPEED_ADC_CHANNEL ADC_CHN_3
+
+#else
 
 #define ADC_CHANNEL_1_PIN PC1
 #define ADC_CHANNEL_2_PIN PC3
@@ -191,10 +202,22 @@
 #define RSSI_ADC_CHANNEL ADC_CHN_3
 #define AIRSPEED_ADC_CHANNEL ADC_CHN_4
 
+#endif
+
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
+
+#ifdef MAMBAH743_2022B
+
+#define PINIO1_PIN PC2
+#define PINIO2_PIN PC5
+
+#else
+
 #define PINIO1_PIN PC5
+
+#endif
 
 // *************** LEDSTRIP ************************
 #define USE_LED_STRIP

sensei-hacker/inav_unofficial_targets

src/main/target/MAMBAH743/target.h
@@ -17,9 +17,17 @@
 
 #pragma once
 
+#ifdef MAMBAH743_2022B
+
+#define TARGET_BOARD_IDENTIFIER "M743"
+#define USBD_PRODUCT_STRING "MAMBAH743_2022B"
+
+#else
 
 #define TARGET_BOARD_IDENTIFIER "M743"
 #define USBD_PRODUCT_STRING "MAMBAH743"
+
+#endif
 
 #define USE_TARGET_CONFIG
 
@@ -40,17 +48,6 @@
 #define SPI1_MISO_PIN PA6
 #define SPI1_MOSI_PIN PA7
 
-#define USE_IMU_ICM42605
-#define IMU_ICM42605_ALIGN      CW0_DEG
-#define ICM42605_SPI_BUS        BUS_SPI1
-#define ICM42605_CS_PIN         PA4
-
-// SPI4 is used on the second MPU6000 gyro, we do not use it at the moment
-// #define USE_SPI_DEVICE_4
-// #define SPI4_SCK_PIN            PE12
-// #define SPI4_MISO_PIN           PE13
-// #define SPI4_MOSI_PIN           PE14
-
 #define USE_IMU_MPU6000
 #define IMU_MPU6000_ALIGN CW180_DEG
 #define MPU6000_SPI_BUS BUS_SPI1
@@ -60,6 +57,22 @@
 #define IMU_BMI270_ALIGN CW180_DEG
 #define BMI270_SPI_BUS BUS_SPI1
 #define BMI270_CS_PIN PA4
+
+#ifdef MAMBAH743_2022B
+
+// SPI4 is used on the second MPU6000 gyro, we do not use it at the moment
+// #define USE_SPI_DEVICE_4
+// #define SPI4_SCK_PIN            PE12
+// #define SPI4_MISO_PIN           PE13
+// #define SPI4_MOSI_PIN           PE14
+
+#define USE_IMU_ICM42605
+#define IMU_ICM42605_ALIGN      CW0_DEG
+#define ICM42605_SPI_BUS        BUS_SPI1
+#define ICM42605_CS_PIN         PA4
+
+#endif
+
 
 // *************** SPI2 OSD ***********************
 #define USE_SPI_DEVICE_2
@@ -165,11 +178,22 @@
 
 #define DEFAULT_RX_TYPE RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER SERIALRX_SBUS
-#define SERIALRX_UART SERIAL_PORT_USART6
 
 // *************** ADC *****************************
 #define USE_ADC
 #define ADC_INSTANCE ADC3
+
+#ifdef MAMBAH743_2022B
+
+#define ADC_CHANNEL_1_PIN PC1
+#define ADC_CHANNEL_2_PIN PC3
+#define ADC_CHANNEL_3_PIN PC0
+
+#define VBAT_ADC_CHANNEL ADC_CHN_1
+#define CURRENT_METER_ADC_CHANNEL ADC_CHN_2
+#define AIRSPEED_ADC_CHANNEL ADC_CHN_3
+
+#else
 
 #define ADC_CHANNEL_1_PIN PC1
 #define ADC_CHANNEL_2_PIN PC3
@@ -181,10 +205,22 @@
 #define RSSI_ADC_CHANNEL ADC_CHN_3
 #define AIRSPEED_ADC_CHANNEL ADC_CHN_4
 
+#endif
+
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
+
+#ifdef MAMBAH743_2022B
+
+#define PINIO1_PIN PC2
+#define PINIO2_PIN PC5
+
+#else
+
 #define PINIO1_PIN PC5
+
+#endif
 
 // *************** LEDSTRIP ************************
 #define USE_LED_STRIP