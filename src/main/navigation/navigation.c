6d6cffbf9dd3f149761b5bd9fa9f42d5268fb6a4
shota <miyafly7@gmail.com>
2022-11-11 04:47:32 +0900

    add sannity check in hot mixprofile switching, and fix cli

--- src/main/navigation/navigation.c
+++ src/main/navigation/navigation.c
@@ -4453,6 +4453,13 @@ bool navigationIsExecutingAnEmergencyLanding(void)
     return navGetCurrentStateFlags() & NAV_CTL_EMERG;
 }
 
+
+bool navigationInAnyMode(void)
+{
+    navigationFSMStateFlags_t stateFlags = navGetCurrentStateFlags();
+    return !(stateFlags ==0);
+}
+
 bool navigationInAutomaticThrottleMode(void)
 {
     navigationFSMStateFlags_t stateFlags = navGetCurrentStateFlags();
